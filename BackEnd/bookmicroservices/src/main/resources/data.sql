d2ce995a83b247d6f764cadf2952da15b70641bc
Alli-st <s3843971@student.rmit.edu.au>
2021-09-04 16:33:56 +1000

    book seed data

---
+++ BackEnd/bookmicroservices/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT INTO book (title, author, isbn) VALUES
+    ('Sun & Steel', 'Yukio Mishima', '9782824901848'),
+    ('Divine Comedy', 'Dante Alighieri', '9788820313500'),
+    ('The Melancholy of Haruhi Suzumiya', 'Nagaru Tanigawa', '9780316039017'),
+    ('The Adventures of Captain Underpants', 'Dav Pilkey', '9780590846271'),
+    ('The Saga of Tanya the Evi Vol. 1 Deus Lo Vult', 'Carlo Zen', '9780316512442'),
+    ('The Day My Bum Went Psycho', 'Andy Griffiths', '9780330400893'),
+    ('Land of the Lustrous Vol. 1', 'Haruko Ichikawa', '9781632364975'),
+    ('Mobile Suit Gundam Thunderbolt Vol. 1', 'Yasuo Ohtagaki', '9784091848109');
