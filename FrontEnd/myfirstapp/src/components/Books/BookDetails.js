9372b751db4f2a6c6b43fe7df34075c488327620
Jeffrey Tan <S3851781@student.rmit.edu.au>
2021-09-10 23:34:36 +1000

    pretty book details

--- FrontEnd/myfirstapp/src/components/Books/BookDetails.js
+++ FrontEnd/myfirstapp/src/components/Books/BookDetails.js
@@ -8,6 +8,7 @@ class BookDetails extends Component {
         super();
         this.state = {
             book: null,
+            listings: null
         }
         //this.handleSort = this.handleSort.bind(this);
         //this.handleSearch = this.handleSearch.bind(this);
@@ -24,12 +25,20 @@ class BookDetails extends Component {
         }) 
 
         // let {isbn} = useParams;
+
         // const searchParams = "?isbn=" + isbn;
         // searchBooks(searchParams)
         //     .then((res) => {
         //         const book = res.data;
         //         this.setState({book: book});
         // })  
+
+        // getListings(isbn)
+        //     .then((res) => {
+        //         const listings = res.data;
+        //         this.setState({listings: listings});
+        //         console.log(this.state.listings)
+        // }) 
     }
 
     render () {
@@ -39,30 +48,68 @@ class BookDetails extends Component {
                 {(this.state.book != null)
                 ? 
                 <div className="content">
+                    <div className="flex-container">
+                        <div className="sub-content">
+                            <img src={"http://covers.openlibrary.org/b/isbn/" + book.isbn + "-L.jpg" }
+                            alt={"book cover for " + book.title}/>
+                            <table>
+                                <tr>
+                                    <th>Author</th>
+                                    <td>{book.author}</td>
+                                </tr>
+                                <tr>
+                                    <th>Genre</th>
+                                    <td>{book.genre}</td>
+                                </tr>
+                                <tr>
+                                    <th>Year Published</th>
+                                    <td>{book.published}</td>
+                                </tr>
+                                <tr>
+                                    <th>ISBN</th>
+                                    <td>{book.isbn}</td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div className="sub-content">
+                            <h1 className="title-bd">{book.title}</h1>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat 
+                                nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa 
+                                qui officia deserunt mollit anim id est laborum.
+                            </p>
+                            <p>
+                                Turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus. Sit amet risus 
+                                nullam eget felis eget. Vestibulum morbi blandit cursus risus at ultrices. Gravida 
+                                cum sociis natoque penatibus et magnis dis parturient. Ultrices in iaculis nunc sed 
+                                augue. Sit amet purus gravida quis blandit. Lectus urna duis convallis convallis. Vel 
+                                pharetra vel turpis nunc eget lorem dolor. Risus feugiat in ante metus dictum at tempor
+                                 commodo. Vel pharetra vel turpis nunc eget lorem dolor. Magna eget est lorem ipsum dolor.
+                                 Posuere sollicitudin aliquam ultrices sagittis orci a scelerisque. Arcu non odio euismod
+                                lacinia at quis risus sed vulputate. Maecenas sed enim ut sem. At imperdiet dui accumsan
+                                 sit. Nam libero justo laoreet sit amet cursus. In ornare quam viverra orci sagittis eu
+                                 volutpat. Sit amet porttitor eget dolor morbi non arcu risus. Adipiscing diam donec
+                                 adipiscing tristique risus nec feugiat.
+                            </p>
+                        </div>
+                    </div>
                     
-                    <div className="sub-content">
-                        <h1 className="title-bd">{book.title}</h1>
-                        <h2 className="author-bd">By {book.author}</h2>
-                    </div>
-                    <div className="sub-content">
-                        <dl>
-                            <dt>Genre</dt>
-                            <dd>{book.genre}</dd>
-                            <dt>Year Published</dt>
-                            <dd>{book.published}</dd>
-                            <dt>ISBN</dt>
-                            <dd>{book.isbn}</dd>
-                        </dl>
-                    </div>
-                    <div className="sub-content">
-                        <img src={"http://covers.openlibrary.org/b/isbn/" + book.isbn + "-L.jpg" }
-                        alt={"book cover for " + book.title}/>
+                    <div className="listings">
+                        <h2>Listings</h2>
+                        <hr/>
+                        {/* {this.state.listings.map((listing) => (
+                            <ListingItem
+                                title = {listing.title}
+                                seller = {listing.seller}
+                                key = {listing.id}
+                            />
+                        ))} */}
                     </div>
 
-                    <div className="sub-content">
-                        <h1>Listings</h1>
-                    </div>
-                    
+
                 </div>
                 : 
                 <div><h2>... waiting for data</h2> </div>
